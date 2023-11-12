a6d193165b35fb72208205484f15eb31cb2585f6
Tayuba <ayubamonnietahiru@gmail.com>
2023-11-12 22:48:31 +0100

    Timers

---
+++ Nucleo_F411RE/PLL_SYSCLK/Core/Inc/main_app.h
@@ -0,0 +1,17 @@
+/*
+ * main.h
+ *
+ *  Created on: 02-Jun-2018
+ *      Author: Ayuba
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include "stm32f4xx_hal.h"
+
+#define SYS_CLOCK_FREQ_50_MHZ 50
+#define SYS_CLOCK_FREQ_64_MHZ 64
+#define SYS_CLOCK_FREQ_84_MHZ 84
+#define SYS_CLOCK_FREQ_100_MHZ 100
+#endif /* MAIN_H_ */
