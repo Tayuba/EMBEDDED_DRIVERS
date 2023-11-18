88f3f5b13ab5a1c5f469b06709142a2cc91b4195
Tayuba <ayubamonnietahiru@gmail.com>
2023-11-18 22:31:28 +0100

    GPIO Drivers

--- Disco_F407/PERIPHERAL_DRIVERS_AND_API_DEVELOPMENTS_MCU-F407/drivers/Src/F407_GPIO_Drivers.c
+++ Disco_F407/PERIPHERAL_DRIVERS_AND_API_DEVELOPMENTS_MCU-F407/drivers/Src/F407_GPIO_Drivers.c
@@ -23,3 +23,23 @@ typedef struct {
 	GPIOx_RegDef_t *pGPIOx;				/* Holds the base address of the GPIO port of which the pin belongs */
 	GPIOx_PinConfig_t GPIO_PinConfig;	/* Holds the pin configuration of GPIOx */
 }GPIO_Handle_t;
+
+/*****************************************************************************************************************
+ * 										APIs Supported Drivers
+ * ***************************************************************************************************************/
+//Clock, Initialization and De-Initialization
+void GPIO_PCLK_Control(void);
+void GPIO_Init(void);
+void GPIO_DeInit(void);
+
+
+//Read, Write data
+void GPIO_ReadFromInputPin(void);
+void GPIO_ReadFromInputPort(void);
+void GPIO_WriteToOutputPin(void);
+void GPIO_WriteToOutputPort(void);
+void GPIO_TogglePin(void);
+
+// IRQ Configuration and Handling
+void GPIO_IRQConfig(void);
+void GPIO_IRQHandling(void);
